2b0e71c60f6fa58a47c81ddb32da594d99f5464f
abdza <abdullah.zainul@gmail.com>
2023-09-11 21:09:21 +0800

    Now will crush food

--- FoodCrush/game.gd
+++ FoodCrush/game.gd
@@ -9,14 +9,16 @@ var foodpics = [
 	'16_burger_dish.png',
 	'19_burrito_dish.png',
 	'21_bagel_dish.png',
-	'23_cheesecake_dish.png',
-	'27_chocolate_dish.png',
-	'29_cookies_dish.png',
+	'23_cheesecake_dish.png'
 	]
 
 var grid = []
 var selected_pic = []
 var curhover = Vector2(0,0)
+var found_same = []
+
+enum GAME_STATE { WAITING, CHECKING, DESTROYING }
+var gamestate = GAME_STATE.WAITING
 
 func changepic(row,col,pic):
 	var nodename = 'food_' + str(row) + '_' + str(col)
@@ -29,16 +31,78 @@ func change_selected(location):
 	var curnode = get_node(nodename)
 	curnode.cur_selected = not curnode.cur_selected
 	
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	for row in range(6):
 		for col in range(5):
 			grid.append(randi() % len(foodpics))
 			changepic(row,col,grid[row * 5 + col])
+	gamestate = GAME_STATE.CHECKING
+	updatestates()
+			
+func updatepics():
+	for row in range(6):
+		for col in range(5):
+			changepic(row,col,grid[row * 5 + col])
 
+func checkgrid(row,col):
+	if gamestate == GAME_STATE.CHECKING:
+		var gridpos = row * 5 + col
+		print("Checking row ",row," and col ",col, " food ",grid[gridpos])
+		if found_same.size()==0:
+			found_same.append({"pos":Vector2(col,row),"food":grid[gridpos]})
+		else:
+			print("Prev food: ",found_same[0].food," Cur food:",grid[gridpos])
+			if found_same[0].food == grid[gridpos]:
+				found_same.append({"pos":Vector2(col,row),"food":grid[gridpos]})
+				print("Found same:",found_same)
+			else:
+				if found_same.size()>=3:
+					print("Will destroy ",found_same)
+					gamestate = GAME_STATE.DESTROYING
+				if gamestate == GAME_STATE.CHECKING:
+					found_same.clear()
+					found_same.append({"pos":Vector2(col,row),"food":grid[gridpos]})
+									
+func updatestates():
+	if gamestate == GAME_STATE.CHECKING:
+		print("In checking rows")
+		for row in range(6):
+			for col in range(5):
+				checkgrid(row,col)
+			if found_same.size()>=3:
+				print("Will destroy ",found_same)
+				gamestate = GAME_STATE.DESTROYING
+			if gamestate == GAME_STATE.CHECKING:
+				found_same.clear()
+	if gamestate == GAME_STATE.CHECKING:
+		print("In checking cols")
+		for col in range(5):
+			for row in range(6):
+				checkgrid(row,col)
+			if found_same.size()>=3:
+				print("Will destroy ",found_same)
+				gamestate = GAME_STATE.DESTROYING
+			if gamestate == GAME_STATE.CHECKING:
+				found_same.clear()
+	if gamestate == GAME_STATE.CHECKING:
+		gamestate = GAME_STATE.WAITING
+	if gamestate == GAME_STATE.DESTROYING:
+		print("Destroying state")
+		if found_same.size()>0:			
+			var curdestroy = found_same.pop_front()
+			print("Destorying ",curdestroy)
+			while curdestroy.pos.y > 0:
+				grid[curdestroy.pos.y * 5 + curdestroy.pos.x] = grid[(curdestroy.pos.y - 1) * 5 + curdestroy.pos.x]
+				curdestroy.pos.y -= 1
+			grid[curdestroy.pos.x] = randi() % len(foodpics)
+			
+		else:
+			gamestate = GAME_STATE.CHECKING
+	updatepics()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	updatestates()
 
 func swap_pic(a,b):
 	print("Will swap ",a," with ",b)
@@ -50,9 +114,10 @@ func swap_pic(a,b):
 	change_selected(a)
 	change_selected(b)
 	selected_pic.clear()
+	gamestate = GAME_STATE.CHECKING
 
 func _input(event):
-	if event is InputEventMouseButton and event.is_pressed():
+	if event is InputEventMouseButton and event.is_pressed() and gamestate == GAME_STATE.WAITING:
 		print("First:",selected_pic)
 		change_selected(curhover)
 		if selected_pic.size()==0:
