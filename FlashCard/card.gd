bd63cdf4f412453f085088abf3b719234deb8cdb
abdza <abdullah.zainul@gmail.com>
2024-03-05 05:41:11 +0800

    Now will choose randomly the cards to test the user

--- FlashCard/card.gd
+++ FlashCard/card.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 enum state { picture, text }
+var mouse_hover
 @export var cur_state : state
 
 # Called when the node enters the scene tree for the first time.
@@ -8,14 +9,30 @@ func _ready():
 	pass # Replace with function body.
 
 func set_picture(pic,name_of_pic):
-	$Polygon2D/picture.texture = load(pic) 
-	$translation.text = name_of_pic
+	$Area2D/Polygon2D/picture.texture = load(pic) 
+	$Area2D/Polygon2D/translation.text = name_of_pic
+	
+func _input(event):
+	if event is InputEventMouseButton and mouse_hover and not event.is_pressed():		
+		if cur_state==state.text:
+			cur_state = state.picture
+		else:
+			cur_state = state.text
+		get_tree().change_scene_to_file("res://flash_card.tscn")
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if cur_state==state.text:
-		$translation.visible = true
-		$Polygon2D/picture.visible = false
+		$Area2D/Polygon2D/translation.visible = true
+		$Area2D/Polygon2D/picture.visible = false
 	else:
-		$translation.visible = false
-		$Polygon2D/picture.visible = true
+		$Area2D/Polygon2D/translation.visible = false
+		$Area2D/Polygon2D/picture.visible = true
+
+
+func _on_area_2d_mouse_entered():
+	mouse_hover = true
+
+
+func _on_area_2d_mouse_exited():
+	mouse_hover = false
--- FlashCard/flash_card.gd
+++ FlashCard/flash_card.gd
@@ -1,13 +1,31 @@
 extends Node2D
 
+var word_choices = [
+	["res://images/car.jpg","Kereta"],
+	["res://images/cat.jpg","Kucing"],
+	["res://images/chicken.png","Ayam"],
+	["res://images/elephant.jpg","Gajah"],
+	["res://images/cow.png","Lembu"]
+]
 
+var word_selection = []
+var word_target 
+
+func randomize_selection():
+	while len(word_selection)<4:
+		var current_word = randi() % len(word_choices)
+		if not current_word in word_selection:
+			word_selection.append(current_word)
+	$card_1.set_picture(word_choices[word_selection[0]][0],word_choices[word_selection[0]][1])
+	$card_2.set_picture(word_choices[word_selection[1]][0],word_choices[word_selection[1]][1])
+	$card_3.set_picture(word_choices[word_selection[2]][0],word_choices[word_selection[2]][1])
+	$card_4.set_picture(word_choices[word_selection[3]][0],word_choices[word_selection[3]][1])
+	word_target = randi() % len(word_selection)	
+	$target.text = word_choices[word_selection[word_target]][1]
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$card_1.set_picture("res://images/car.jpg","Kereta")
-	$card_2.set_picture("res://images/cat.jpg","Kucing")
-	$card_3.set_picture("res://images/chicken.png","Ayam")
-	$card_4.set_picture("res://images/elephant.jpg","Gajah")
-
+	randomize_selection()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
